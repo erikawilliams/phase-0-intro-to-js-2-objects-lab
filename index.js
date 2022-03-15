bc1f9eba0693b8a8df8bdf36aab953d0f82c0ed8
Erika Williams <erika@erikawilliams.us>
2022-03-15 16:45:10 -0600

    commit tree 10e17f47e303c3c58895d033dfc81403fdcb9f37

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {name: "erika", streetAddress:"123 main"}
+
+function updateEmployeeWithKeyAndValue (employee,key,value){
+    const newEmps = {...employee}
+    newEmps[key]=value
+    return newEmps
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value
+    return employee
+}
+function deleteFromEmployeeByKey(employee,key){
+    const newEmps = {...employee}
+    delete newEmps[key];
+    return newEmps
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+}
